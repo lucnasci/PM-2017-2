9e93b2d86a61b2bec03b376288da3006734af5ad
Rodrigo Marques <rodrigo.marques@uniriotec.br>
2017-11-25 17:04:36 -0200

    Added enum to have disciplines' results colors

---
+++ src/main/java/DisciplineResultColorEnum.java
@@ -0,0 +1,21 @@
+
+public enum DisciplineResultColorEnum {
+	APROVADO("#94EF00"), REPROVADO("#FF1800");
+
+	private String text;
+
+	DisciplineResultColorEnum(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return this.text;
+	}
+
+	public static DisciplineResultColorEnum fromString(String text) {
+		for (DisciplineResultColorEnum b : DisciplineResultColorEnum.values())
+			if (b.text.equalsIgnoreCase(text))
+				return b;
+		return null;
+	}
+}
