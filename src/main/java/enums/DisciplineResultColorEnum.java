09cb2ab5fb8da748bf28dd464c2a82b487e0921a
Rodrigo Marques <romadasilvax@gmail.com>
2017-11-29 13:57:40 -0200

    Refactoring and revisioning enums

--- src/main/java/enums/DisciplineResultColorEnum.java
+++ src/main/java/enums/DisciplineResultColorEnum.java
@@ -5,18 +5,32 @@ public enum DisciplineResultColorEnum {
 
 	private String text;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param text
+	 *            Set the DisciplineResultColorEnum name based on the given text
+	 */
 	DisciplineResultColorEnum(String text) {
 		this.text = text;
 	}
 
+	/**
+	 * @return the name of the DisciplineResultColorEnum
+	 */
 	public String getText() {
 		return this.text;
 	}
 
+	/**
+	 * @param text
+	 * @return the DisciplineResultColorEnum equivalent for the given text. If
+	 *         there's none returns null
+	 */
 	public static DisciplineResultColorEnum fromString(String text) {
-		for (DisciplineResultColorEnum b : DisciplineResultColorEnum.values())
-			if (b.text.equalsIgnoreCase(text))
-				return b;
+		for (DisciplineResultColorEnum disciplineResultColorEnum : DisciplineResultColorEnum.values())
+			if (disciplineResultColorEnum.text.equalsIgnoreCase(text))
+				return disciplineResultColorEnum;
 		return null;
 	}
 }
--- src/main/java/enums/DisciplineResultEnum.java
+++ src/main/java/enums/DisciplineResultEnum.java
@@ -1,30 +1,37 @@
 package enums;
-// PRECISA CRIAR UM VALOR PARA CASO A DISCIPLINA NÃO ESTEJA LISTADA NO HISTORICO
-// OU SEJA, CASO O VALOR PASSADO PARA O ENUM SEJA NULL.
 
 public enum DisciplineResultEnum {
-	MATRICULA("ASC"),
-	APROVADO("APV"),
-	REPROVADO("REP"),
-	REPROVADO_POR_FALTA("REF"),
-	DISPENSADO_COM_NOTA("ADI"),
-	DISPENSADO_SEM_NOTA("DIS"),
-	NAO_CURSADA("");
-	
+	MATRICULA("ASC"), APROVADO("APV"), REPROVADO("REP"), REPROVADO_POR_FALTA("REF"), DISPENSADO_COM_NOTA(
+			"ADI"), DISPENSADO_SEM_NOTA("DIS"), NAO_CURSADA("");
+
 	private String text;
-	
+
+	/**
+	 * Constructor
+	 * 
+	 * @param text
+	 *            Set the DisciplineResultColorEnum name based on the given text
+	 */
 	DisciplineResultEnum(String text) {
 		this.text = text;
 	}
 	
+	/**
+	 * @return the name of the DisciplineResultColorEnum
+	 */
 	public String getText() {
 		return this.text;
 	}
-	
+
+	/**
+	 * @param text
+	 * @return the DisciplineResultColorEnum equivalent for the given text. If
+	 *         there's none returns null
+	 */
 	public static DisciplineResultEnum fromString(String text) {
-	    for (DisciplineResultEnum b : DisciplineResultEnum.values()) 
-	    	if (b.text.equalsIgnoreCase(text)) 
-	    		return b;
-	    return null;
-	 }
+		for (DisciplineResultEnum b : DisciplineResultEnum.values())
+			if (b.text.equalsIgnoreCase(text))
+				return b;
+		return null;
+	}
 }
